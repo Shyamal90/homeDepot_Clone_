332b78c674f3e24d9fe877a503112fa849d31d93
Shyamal Sardar <88072892+Shyamal90@users.noreply.github.com>
2022-01-18 13:07:17 +0530

    successful connection with db

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const User = require("../models/user.model");
+
+const router = express.Router();
+
+router.post("/",async(req,res)=>{
+    try {
+        const user = await User.create(req.body);
+        console.log(user);
+        res.status(201).json({user});
+    } catch (error) {
+        console.log(error);
+        res.status(404).send(error.message);
+    }
+})
+
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-const express=require("express")
+const express=require("express");
+
+
+
+
 const app=express()
-const connect=require("./configs/db")
-
-
-
 app.use(express.json())
 
-const productController=require("./controllers/product.controller")
+const userController = require("./controllers/user.controller");
+const productController=require("./controllers/product.controller");
 
 app.use("/products",productController)
+app.use("/users",userController);
 
-module.exports=app
+
+
+
+module.exports=app;
